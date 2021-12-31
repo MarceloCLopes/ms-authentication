e1d5d71d272930e6e835ba5a339f7234ad1c553a
MarceloCLopes <marc01mcl@gmail.com>
2021-12-31 09:07:46 -0300

    Finalização do servidor e Rotas Usuários

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
-import express, { Request, Response, NextFunction } from "express";
+import express from "express";
+import statusRoute from "./routes/status.routes";
+import usersRoute from "./routes/users.routes";
 
 const app = express();
 
-app.get("/status", (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).send({ foo: "sucesso total!, muito bem" });
-});
+// Configuração da Aplicação
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+// Configuração de Rotas
+app.use(statusRoute);
+app.use(usersRoute);
+
+// Inicialização do Servidor
 app.listen(3000, () => {
   console.log("Aplicação executando na porta 3000");
 });
